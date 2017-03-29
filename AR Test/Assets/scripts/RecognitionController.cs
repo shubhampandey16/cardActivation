60068168c76b442b60475f2a30e8d18747364767
Michael Cooper <Cooper.MichaelCS@gmail.com>
2017-03-28 19:20:56 -0700

    getting all files this time!

---
+++ AR Test/Assets/scripts/RecognitionController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+//probably attach something similar to this to each image attached to the card being recognized
+
+
+public class RecognitionController : MonoBehaviour,
+ITrackableEventHandler
+{
+	private TrackableBehaviour mTrackableBehaviour;
+	private Animator anim;
+	public bool detected;
+
+	void Start()
+	{
+		detected = false;
+		anim = GetComponent<Animator> ();
+
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			//GameObject.Find ("merchTrans1").GetComponent<merchTrans1Anim> ().TriggerAnimation ();
+			detected = true;
+			Debug.Log ("detected = " + detected);
+
+
+
+			// Play audio when target is found
+			//audio.Play();
+		}
+		else
+		{
+			// Stop audio when target is lost
+			//audio.Stop();
+
+			detected = false;
+			Debug.Log ("detected = " + detected);
+
+		}
+	}   
+}
